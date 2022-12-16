3d336c14d141ad036efad1e288fe5d4a85deb1e1
omaran bazna <omran4d@gmail.com>
2022-12-16 11:20:45 -0500

    app the routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/model" element={<div>Model</div>} />
+        <Route path="/login" element={<div>Log In</div>} />
+        <Route path="/" element={<div>Welcome</div>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
