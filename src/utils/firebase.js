2d99711e7251535f04da5f029e9668fbc6a413e2
omaran bazna <omran4d@gmail.com>
2022-12-16 11:23:13 -0500

    add the firebase to the application

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGL98TuYt6jAhUoIuMHK9Cb8GPONRanMI",
+  authDomain: "modeller-58be3.firebaseapp.com",
+  projectId: "modeller-58be3",
+  storageBucket: "modeller-58be3.appspot.com",
+  messagingSenderId: "2605093847",
+  appId: "1:2605093847:web:4e94bd955c23b610ca3192",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
